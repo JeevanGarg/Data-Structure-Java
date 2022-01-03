574a4dab57cbba78fab31bb55c90fd1a7b395916
JeevanGarg <luckyjeevangarg89@gmail.com>
2022-01-03 16:23:53 +0530

    Array

--- Array/ArrayIntersection.java
+++ Array/ArrayIntersection.java
@@ -1,60 +1,58 @@
-//Find duplicate element ina an array
-
+import java.util.Scanner;
+import java.lang.reflect.Array;
 import java.util.*;
+
 
 public class ArrayIntersection {
 
-    public static int[] takeInput(){
+    public static int[] takeInput()
+    {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of Array");
         int n=sc.nextInt();
-        int[] a=new int[n];
 
+        int[] a=new int[n];
+        
         for(int i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
-
         }
-        return a;
 
+        return a;
     }
 
     public static void main(String[] args){
-
-        int[] a=takeInput();     
+        int[] a=takeInput();
         int[] b=takeInput();
         ArrayList<Integer> arr=new ArrayList<>();
 
         Arrays.sort(a);
         Arrays.sort(b);
-
-        int l1=a.length-1;
-        int l2=b.length-1;
         int i=0,j=0;
+        int l1=a.length,l2=b.length;
 
-        while(i<l1 && j<l2){
-            if(a[i]==b[j])
-            {
+        while(i<l1 && j<l2)
+        {
+            if(a[i]==b[j]){
                 arr.add(a[i]);
                 i++;
                 j++;
             }
-            else if(a[i]<b[i]){
-                
+
+            else if(a[i]<b[j])
+            {
                 i++;
+
             }
+
             else{
                 j++;
             }
+
         }
 
         for(int k:arr){
-            System.out.print(k);
+            System.out.println(k);
         }
-
-        
-
-        
     }
     
 }
