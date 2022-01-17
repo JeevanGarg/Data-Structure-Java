b958855ffab1e0b80d03c12a2b5a74f09115de7e
JeevanGarg <luckyjeevangarg89@gmail.com>
2022-01-17 17:08:38 +0530

    HashMap

---
+++ Hashmap/maxFrequencyNumber.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.*;
+public class Solution {
+
+    public static int maxFrequencyNumber(int[] arr)
+    {
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int max=Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                int val=map.get(arr[i]);
+                val++;
+                map.put(arr[i],val);
+            }
+            map.put(arr[i],1);
+        }
+        
+        Set<Integer> key=map.keySet();
+        
+        for(int i:key){
+            if(max<=map.get(i)){
+                max=i;
+                
+            }
+        }
+        return max;
+    }
+}
