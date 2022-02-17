c8f71cd3c94018ec0b841aae31d413af28685432
JeevanGarg <luckyjeevangarg89@gmail.com>
2022-02-17 14:57:33 +0530

    Array

---
+++ Array/demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.*;
+
+public class demo {
+
+    public static void main(String[] args)
+    {
+       System.out.println("Hello World");
+    }
+    
+}
